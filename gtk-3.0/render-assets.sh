daebb2c4fb7097aa31959385fe9f1b91fb47f87a
Sebastian Riley Rasor <https://www.sebastianrasor.com/contact>
2021-10-15 16:04:38 -0400

    init

---
+++ gtk-3.0/render-assets.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+INKSCAPE="inkscape"
+
+SRC_FILE="assets.svg"
+ASSETS_DIR="assets"
+INDEX="assets.txt"
+
+for i in `cat $INDEX`
+do 
+if [ -f $ASSETS_DIR/$i.png ]; then
+    echo $ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null #\
+fi
+if [ -f $ASSETS_DIR/$i@2.png ]; then
+    echo $ASSETS_DIR/$i@2.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i@2.png
+    $INKSCAPE --export-id=$i \
+              --export-dpi=180 \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null #\
+fi
+done
+exit 0
